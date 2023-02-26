cf76b8b9e5c808e7d65e15feecbac687e4b5fbd4
AshKitturGitHub <ashtonkittur@gmail.com>
2023-02-26 18:17:09 -0500

    :wq

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 import mongoose from 'mongoose';
-import Blog from '.model/Blog';
+import dataEntry from '.model/dataEntry';
 
 mongoose.connect("mongodb+srv://ashKittur:8KCwteOgxFsWPcGQ@cluster0.u5jjh5r.mongodb.net/test")
 
 // Create a new blog post object
-const article = new Blog({
-    title: 'Awesome Post!',
-    slug: 'awesome-post',
-    published: true,
-    content: 'This is the best post ever',
-    tags: ['featured', 'announcement'],
+const entry = new Entries({
+  success: null,
+  _id: "example-id",
+  patientId: "example-patientID",
+  age: 0,
+  sex: "example-sex",
+  zipCode: "example-zipCode",
+  bmi: 0,
+  __v: 0,
+  examId: "example-examID",
+  keyFindings: "example-keyFindings",
+  brixiaScores: 0,
+  imageURL: "example-imageURL",
+  exams: [{
+    _id: "example-id",
+    patientId: "example-patientID",
+    age: 0,
+    sex: "example-sex",
+    zipCode: "example-zipCode",
+    bmi: 0,
+    __v: 0,
+    examId: "example-examID",
+    keyFindings: "example-keyFindings",
+    brixiaScores: 0,
+    imageURL: "example-imageURL"
+  }]
   });
   
   // Insert the article in our MongoDB database
-  await article.save();
+  await entry.save();
