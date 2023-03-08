e57dc294b1aaee8b089b13526cb5e9df66c6462a
Ayomide Oladele <89360509+Ayomide11111@users.noreply.github.com>
2023-03-08 00:49:15 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -35,7 +35,7 @@ export default function App() {
   return( 
     <div>
       <BrowserRouter>
-        <Navbar />
+        <NavBar />
         <div className='pages'>
           <Routes>
             <Route
