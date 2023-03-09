86c5fcf040a27e0f1afc0302375495882726a0e4
Alfonso Meraz <merazalfonso2@gmail.com>
2023-03-09 08:54:07 -0800

    fixed api connected to frontend

--- api/app.js
+++ api/app.js
@@ -20,14 +20,14 @@ var app = express();
 
 // routes
 app.use('/api/exams', examRoutes);
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
+      console.log('connected to db & listening on port')
     })
   })
   .catch((error) => {
