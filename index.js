ccf1703be318520d10e47e7115e6c1afb58127cd
AshKitturGitHub <ashtonkittur@gmail.com>
2023-03-10 10:11:32 -0500

    Test Message

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 import dataEntry from '.model/dataEntry';
-
-mongoose.connect("mongodb+srv://ashKittur:8KCwteOgxFsWPcGQ@cluster0.u5jjh5r.mongodb.net/test")
+const PORT = process.env.PORT || 3001;
+const url = "mongodb+srv://ashKittur:8KCwteOgxFsWPcGQ@cluster0.u5jjh5r.mongodb.net/test"
+mongoose.connect(url, { useNewUrlParser: true})
 
 // Create a new blog post object
 const entry = new Entries({
@@ -32,5 +33,35 @@ const entry = new Entries({
   }]
   });
   
+  // Check for successful connection
+  const db = mongoose.connection
+  db.on('error', (error) => console.error(error))
+  db.once('open', () => console.log('Connected to database'))
+  // App is using json
+  app.use(express.json())
+
+  // App using routers
+  const indexRouter = require('./routes/index')
+  app.use('/index', indexRouter)
+  const examsRouter = require('./routes/exams')
+  app.use('/exams', examsRouter)
+  const usersRouter = require('./routes/users')
+  app.use('/users', usersRouter)
+  
+  app.get("/", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
+  if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(favicon(__dirname + 'client/build/favicon.ico'));
+    app.get('*', function(req, res){
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
   // Insert the article in our MongoDB database
   await entry.save();
